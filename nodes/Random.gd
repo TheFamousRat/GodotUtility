69a11f49cd74b2c93f8a92a3e0f7d81970c5a8d8
Guillaume Roy <gpotato.gr@gmail.com>
2019-07-04 16:23:58 +0000

    Removed _distribution in functions

--- nodes/Random.gd
+++ nodes/Random.gd
@@ -6,40 +6,40 @@ extends Node
 
 #Returns a value from a uniform distribution
 #Parameters are the interval. Value has an equal chance to take any value of this interval
-func uniform_distribution(min_ : float = 0.0, max_ : float = 1.0, continuous : bool = true):
+func uniform(min_ : float = 0.0, max_ : float = 1.0, continuous : bool = true):
 	if continuous:
 		return rand_range(min_,max_)
 	else:
 		return int(min_) + randi()%(int(max_) - int(min_) + 1)
 
-func chi2_distribution(degOfFreedom : int) -> float:
+func chi2(degOfFreedom : int) -> float:
 	var sum : float = 0.0
 	for i in range(0,degOfFreedom):
-		sum += pow(normal_distribution(0.0,1.0),2.0)
+		sum += pow(normal(0.0,1.0),2.0)
 	return sum
 
 #1 or 0
-func bernoulli_distribution(prob : float) -> int:
+func bernoulli(prob : float) -> int:
 	return int(randf() < prob)
 
-func binomial_distribution(tries : int, prob : float, simulated : bool):
+func binomial(tries : int, prob : float, simulated : bool):
 	if tries < 100 or simulated:
 		var acc : int = 0
 		for i in range(0,tries):
-			acc += bernoulli_distribution(prob)
+			acc += bernoulli(prob)
 		return acc
 	else:
-		return int(round(abs(normal_distribution(tries*prob,tries*prob*(1.0-prob)))))
+		return int(round(abs(normal(tries*prob,tries*prob*(1.0-prob)))))
 
-func exponential_distribution(param : float = 1.0) -> float:
+func exponential(param : float = 1.0) -> float:
 	return ln(1.0 - randf())/(-param)
 
 #Returns a value from a variable following a random gaussian distribution.
 #Parameters are average and standard deviation
-func normal_distribution(avg : float = 0.0, sd : float = 1.0) -> float:
+func normal(avg : float = 0.0, sd : float = 1.0) -> float:
 	return avg+sd*sqrt(-2*(ln(randf())))*cos(2*PI*randf())
 
-func geometric_distribution(prob : float) -> int:
+func geometric(prob : float) -> int:
 	return int(ln(1-randf())/ln(1-prob))
 
 #Natural logarithm
