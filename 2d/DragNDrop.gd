b52fc12d8ff1f53ab09f6c24ba5dda9d7d6d7929
Guillaume Roy <gpotato.gr@gmail.com>
2019-07-05 22:49:32 +0000

    Added possibility to set a rest position to the dragNDrop
    Once the DragNDrop gets released, it will return to its rest pos

--- 2d/DragNDrop.gd
+++ 2d/DragNDrop.gd
@@ -1,23 +1,36 @@
-#A simple Button that gets dragged when clicked on
-#The speed at which it follows the mouse can be changed by "DragSpeed"
-
 extends TextureButton
 
 var dragging : bool = false
 var originalMouseDelta : Vector2
-onready (float) var dragSpeed = INF
+var dragSpeed : float = 3.0
+var restPos : Vector2
+
+export (bool) var enableRestPos = false
+
+func _ready():
+	restPos = self.rect_position
 
 func _on_DragNDrop_button_down():
+	restPos = self.rect_position
 	dragging = true
 	originalMouseDelta = get_viewport().get_mouse_position() - self.rect_position
+	Global.currentDragNDrop = self
 
 func _on_DragNDrop_button_up():
 	dragging = false
+	if Global.currentDragNDrop == self:
+		Global.currentDragNDrop = null
 
 func _process(delta):
 	if dragging:
 		var target : Vector2 = get_viewport().get_mouse_position() - originalMouseDelta
-		if (rect_position - target).length() < dragSpeed:
-			rect_position = target
-		else:
-			rect_position -= dragSpeed * (rect_position - target).normalized()
+		rect_position = interpolatePos(rect_position, target, dragSpeed)
+	elif enableRestPos:
+		if self.rect_position != restPos:
+			rect_position = interpolatePos(rect_position, restPos, dragSpeed)
+
+func interpolatePos(pos, targetPos, speed : float):
+	if (pos - targetPos).length() < speed:
+		return targetPos
+	else:
+		return pos -(speed * (pos - targetPos).normalized())
