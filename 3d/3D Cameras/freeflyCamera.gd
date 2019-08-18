3bb20ef307537b23da4c1ea220b147327386ccfd
Guillaume Roy <gpotato.gr@gmail.com>
2019-08-18 22:11:10 +0000

    Improved FreeFlyCamera

--- 3d/3D Cameras/freeflyCamera.gd
+++ 3d/3D Cameras/freeflyCamera.gd
@@ -1,12 +1,12 @@
 # Code by Guillaume Roy, 2019
-# This script is a code for a basic "flying camera". The camera's orientation is guided by the mouse, and its position by the 
+# This script is a code for a basic "flying camera". The camera's orientation is guided by the mouse, and its position by the
 # directional keys
 
 extends Camera
 
 export (bool) var movEnabled = true
 export (float) var mouseSensitivity = 0.5
-export (float) var flyspeed = 1.0
+export (float) var flyspeed = 100.0
 
 var yaw : float = 0.0
 var pitch : float = 0.0
@@ -14,20 +14,21 @@ var pitch : float = 0.0
 func _ready():
 	yaw = 0.0
 	pitch = 0.0
-	
+
 func _input(event):
 	if event is InputEventMouseMotion and movEnabled:
 		var mouseVec : Vector2 = event.get_relative()
-		
+
 		yaw = fmod(yaw  - mouseVec.x * mouseSensitivity , 360)
 		pitch = max(min(pitch - mouseVec.y * mouseSensitivity , 90), -90)
 		self.set_rotation(Vector3(deg2rad(pitch), deg2rad(yaw), 0))
-		
-		if(Input.is_action_pressed("ui_up")):
-			self.set_translation(self.get_translation() - get_global_transform().basis*Vector3(0,0,1) * flyspeed * .01)
-		if(Input.is_action_pressed("ui_down")):
-			self.set_translation(self.get_translation() - get_global_transform().basis*Vector3(0,0,1) * flyspeed * -.01)
-		if(Input.is_action_pressed("ui_left")):
-			self.set_translation(self.get_translation() - get_global_transform().basis*Vector3(1,0,0) * flyspeed * .01)
-		if(Input.is_action_pressed("ui_right")):
-			self.set_translation(self.get_translation() - get_global_transform().basis*Vector3(1,0,0) * flyspeed * -.01)
+
+func _process(delta):
+	if(Input.is_action_pressed("ui_up")):
+		self.set_translation(self.get_translation() - get_global_transform().basis*Vector3(0,0,1) * delta * flyspeed * .01)
+	if(Input.is_action_pressed("ui_down")):
+		self.set_translation(self.get_translation() - get_global_transform().basis*Vector3(0,0,1) * delta * flyspeed * -.01)
+	if(Input.is_action_pressed("ui_left")):
+		self.set_translation(self.get_translation() - get_global_transform().basis*Vector3(1,0,0) * delta * flyspeed * .01)
+	if(Input.is_action_pressed("ui_right")):
+		self.set_translation(self.get_translation() - get_global_transform().basis*Vector3(1,0,0) * delta * flyspeed * -.01)
